6f1960634820fbff026e85fa9b51eb66994ad2ac
nikballal <nikhil.ballal1994@gmail.com>
2022-08-31 12:21:58 +1000

    homepage banner and prod connected with sanity

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+//The front page of the e-commerce store
+import React from "react";
+import { client } from "../lib/client";
+import { Product, FooterBanner, HeroBanner } from "../components";
+
+const Home = ({ products, bannerData }) => {
+  return (
+    <>
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div className="products-container">
+        {products?.map((product) => product.name)}
+      </div>
+      <FooterBanner />
+    </>
+  );
+};
+
+//to fetch data from the server (using next js)
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]'; //get all products from sanity dashboard
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]'; //get all banners from sanity dashboard
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
+
+export default Home;
